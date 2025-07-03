3c564b8ae53f8b432b99ba1b72d54a64086f0387
Ricardo Adalberto Romero Medina <42826880+Adal612Git@users.noreply.github.com>
2025-07-02 20:09:52 -0600

    Fix cart initialization

--- tortillas/js/carrito.js
+++ tortillas/js/carrito.js
@@ -147,13 +147,6 @@ if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', initCarrito);
 } else {
   initCarrito();
-  });
-}
-
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', setupCart);
-} else {
-  setupCart();
 }
 
 window.carrito = carrito;
